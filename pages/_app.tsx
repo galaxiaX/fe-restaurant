5ecf279e2f21e26c1232c9f1a0378785e4fe1bd4
Namo <pongsathon139@gmail.com>
2023-04-27 21:23:34 +0700

    fixing cors and google font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/store";
-import axios from "axios";
 import "@/styles/globals.css";
-
-axios.defaults.baseURL =
-  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8080";
-axios.defaults.withCredentials = true;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import Body from "@/components/Body";
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Head from "next/head";
+
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8080";
+axios.defaults.withCredentials = true;
 
 const Home = () => {
   return (
@@ -8,9 +13,9 @@ const Home = () => {
       <Head>
         <title>FoodScout</title>
         <link
-          href="https://fonts.googleapis.com/css?family=Sarabun"
+          href="https://fonts.googleapis.com/css?family=Sarabun&display=optional"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
       <Layout>
         <Body />
