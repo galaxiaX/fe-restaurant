6ffa34c1b08d8335c083fd6384d3bdaedac33656
Namo <pongsathon139@gmail.com>
2023-04-27 21:33:27 +0700

    fixing cors

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://api.example.com/:path*",
+        destination: "https://placesearch-back/:path*",
       },
     ];
   },
